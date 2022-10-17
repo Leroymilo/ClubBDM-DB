20501ef1f3571cc1dfc1befca60f879bcfab2489
leroymilo <leroyhaveamailbox@gmail.com>
2022-10-17 08:52:58 +0200

    series authors done

--- linked_classes/series_add.py
+++ linked_classes/series_add.py
@@ -27,18 +27,52 @@ class Series (SeriesWindow) :
             self.scroll_auth, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
             [""] + list(self.auth_dict.keys()), wx.CB_SORT )
         new_choice.Select(0)
+        new_choice.SetId(1100+len(self.auth_choices))
         self.auth_sizer.Add( new_choice, 0, wx.ALL|wx.EXPAND, 5 )
+        self.auth_choices.append(new_choice)
         new_choice.Bind(wx.EVT_CHOICE, self.auth_selected)
     
     def auth_selected(self, event: wx.Event) :
-        print("Selection :", event.GetEventObject().GetSelection())
+        choice_i = event.GetEventObject().GetId() - 1100
+        if self.auth_choices[choice_i].GetSelection() == 0  and choice_i != len(self.auth_choices) - 1 :
+            for i in range(choice_i, len(self.auth_choices) - 1) :
+                self.auth_choices[i].SetSelection(self.auth_choices[i+1].GetSelection())
+            
+            if len(self.auth_choices) > 1 :
+                last_choice = self.auth_choices.pop()
+                self.auth_sizer.Remove(last_choice.GetId() - 1100)
+                last_choice.Destroy()
+        
+        elif self.auth_choices[choice_i].GetSelection() != 0 and choice_i == len(self.auth_choices) - 1 :
+            self.add_auth_ch()
+        
+        self.Layout()
     
     def add_edit_ch(self) :
         new_choice = wx.Choice(
             self.scroll_edit, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
             [""] + list(self.edit_dict.keys()), wx.CB_SORT )
         new_choice.Select(0)
+        new_choice.SetId(1200+len(self.edit_choices))
         self.edit_sizer.Add( new_choice, 0, wx.ALL|wx.EXPAND, 5 )
+        self.edit_choices.append(new_choice)
+        new_choice.Bind(wx.EVT_CHOICE, self.edit_selected)
+    
+    def edit_selected(self, event: wx.Event) :
+        choice_i = event.GetEventObject().GetId() - 1200
+        if self.edit_choices[choice_i].GetSelection() == 0  and choice_i != len(self.edit_choices) - 1 :
+            for i in range(choice_i, len(self.edit_choices) - 1) :
+                self.edit_choices[i].SetSelection(self.edit_choices[i+1].GetSelection())
+            
+            if len(self.edit_choices) > 1 :
+                last_choice = self.edit_choices.pop()
+                self.edit_sizer.Remove(last_choice.GetId() - 1200)
+                last_choice.Destroy()
+        
+        elif self.edit_choices[choice_i].GetSelection() != 0 and choice_i == len(self.edit_choices) - 1 :
+            self.add_edit_ch()
+        
+        self.Layout()
     
     def add_series(self, event) :
         if self.book_cat_choice.GetSelection() == -1 :
